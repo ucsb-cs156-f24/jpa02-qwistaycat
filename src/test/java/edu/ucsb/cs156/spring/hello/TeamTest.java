f72591d65b47a1e203f801c3b312e65331c87a31
Christy Yu <christyyu@ucsb.edu>
2024-10-09 14:35:54 -0700

    save

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -18,9 +18,38 @@ public class TeamTest {
     public void getName_returns_correct_name() {
        assert(team.getName().equals("test-team"));
     }
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+    @Test
+    public void equals_returns_correct_string() {
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+        assertEquals(false, team.equals("string"));
+
+        Team team2 = new Team("test-team");
+        assertEquals(true, team.equals(team2));
+
+        Team team3 = new Team("test-team2");
+        assertEquals(false, team.equals(team3));
+
+        Team team4 = new Team("test-team4");
+        team4.addMember("Christy");
+        assertEquals(false, team.equals(team4));
+
+        Team team5 = new Team("test-team");
+        team5.addMember("Christy");
+        assertEquals(false, team.equals(team5));
+    }
+    @Test
+    public void hashCode_returns_correct_string() {
+        // instantiate t as a Team object
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 0;
+        assertEquals(expectedResult, result);
+     
+    }
+    
 
 }
